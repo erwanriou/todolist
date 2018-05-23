bf0ccf01a0b70c7f6ec138651e9c7d623129a09c
Erwan Riou <rr.1.riou@gmail.com>
2018-05-23 17:11:08 +0200

    fixing the redux dev tool implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App'
 import reducer from './reducers'
 import middleware from './middleware'
 
-const store = createStore(reducer, composeWithDevTools(
-  applyMiddleware(...middleware),
-  // other store enhancers if any
-));
+const store = createStore(reducer, composeWithDevTools(middleware));
 
 ReactDOM.render(
   <Provider store={store}>
